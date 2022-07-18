9bf8e552a28c3f9a857da4b4b12d4e401260b81f
Flávio <flavio.pasantos@outlook.com>
2022-07-17 22:16:13 -0300

    stc: Create initial folder and structure

---
+++ atividades/src/App.jsx
@@ -0,0 +1,22 @@
+
+import "./App.css";
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <div>
+        <ul className="list-group">
+          <li className="list-group-item">Atividade 1</li>
+          <li className="list-group-item">Atividade 2</li>
+          <li className="list-group-item">Atividade 3</li>
+          <li className="list-group-item">Atividade 4</li>
+          <li className="list-group-item">Atividade 5</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
