1c2a2949c50a2b2dad379488428cbfb8b1ab2425
Flávio <flavio.pasantos@outlook.com>
2022-07-19 22:19:03 -0300

    feat: insert feature

--- atividades/src/App.jsx
+++ atividades/src/App.jsx
@@ -1,18 +1,59 @@
-
-import "./App.css";
+import { useState } from 'react';
+import './App.css';
 
 function App() {
+  const [atividade_list, setAtividade_in_list] = useState([
+    {
+      id: 1,
+      descricao: 'Isto é uma ativade',
+    },
+    {
+      id: 2,
+      descricao: 'Isto é uma atividade',
+    },
+  ]);
+  const addAtividade = (e) => {
+    e.preventDefault();
 
+    const atividade = {
+      id: document.getElementById('id').value,
+      descricao: document.getElementById('descricao').value,
+    };
+    setAtividade_in_list([...atividade_list, { ...atividade }]);
+    console.log(atividade_list);
+  };
 
   return (
-    <div className="App">
+    <div className='App'>
+      <form className='row g-3'>
+        <div className='col-md-6'>
+          <label htmlFor='descricao' className='form-label'>
+            Descrição
+          </label>
+          <input type='text' className='form-control' id='descricao' />
+        </div>
+
+        <div className='col-md-6'>
+          <label htmlFor='id' className='form-label'>
+            Id
+          </label>
+          <input type='text' className='form-control' id='id' />
+        </div>
+
+        <br />
+
+        <button className='btn btn-outline-primary' onClick={addAtividade}>
+          + Inserir
+        </button>
+      </form>
+      <br />
       <div>
-        <ul className="list-group">
-          <li className="list-group-item">Atividade 1</li>
-          <li className="list-group-item">Atividade 2</li>
-          <li className="list-group-item">Atividade 3</li>
-          <li className="list-group-item">Atividade 4</li>
-          <li className="list-group-item">Atividade 5</li>
+        <ul className='list-group'>
+          {atividade_list.map((a) => (
+            <li key={a.id} className='list-group-item'>
+              {a.id} - {a.descricao}
+            </li>
+          ))}
         </ul>
       </div>
     </div>
