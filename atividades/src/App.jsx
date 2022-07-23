f0288039c6343fa430aa438fe4b701cc80471a55
Flavio Santos <flavio.pasantos@outlook.com>
2022-07-23 20:23:27 -0300

    refix

--- atividades/src/App.jsx
+++ atividades/src/App.jsx
@@ -13,6 +13,7 @@ function App() {
           <li className="list-group-item">Atividade 3</li>
           <li className="list-group-item">Atividade 4</li>
           <li className="list-group-item">Atividade 5</li>
+          <li className="list-group-item">Atividade 5</li>
         </ul>
       </div>
     </div>
